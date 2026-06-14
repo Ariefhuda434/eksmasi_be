396e30180f635a2ddb2215359acbeb6860f6f5e0
Muhammad Arief Huda <ariefhuda434@gmail.com>
2026-06-14 12:51:21 +0700

    fix: parse MYSQL_URL manually

--- config/db.js
+++ config/db.js
@@ -4,8 +4,13 @@ const mysql = require('mysql2')
 let db
 
 if (process.env.MYSQL_URL) {
+  const url = new URL(process.env.MYSQL_URL)
   db = mysql.createPool({
-    uri: process.env.MYSQL_URL,
+    host: url.hostname,
+    port: url.port,
+    user: url.username,
+    password: url.password,
+    database: url.pathname.replace('/', ''),
     waitForConnections: true,
     connectionLimit: 10,
     ssl: { rejectUnauthorized: false }
