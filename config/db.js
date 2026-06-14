042bcd175198867180d320df324abcb0101c5d73
Muhammad Arief Huda <ariefhuda434@gmail.com>
2026-06-14 12:55:20 +0700

    fix: use Railway MYSQL* variables

--- config/db.js
+++ config/db.js
@@ -3,19 +3,20 @@ const mysql = require('mysql2')
 
 let db
 
-if (process.env.MYSQL_URL) {
-  const url = new URL(process.env.MYSQL_URL)
+if (process.env.MYSQLHOST) {
+  // Railway
   db = mysql.createPool({
-    host: url.hostname,
-    port: url.port,
-    user: url.username,
-    password: url.password,
-    database: url.pathname.replace('/', ''),
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT || 3306,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     ssl: { rejectUnauthorized: false }
   })
 } else {
+  // Lokal
   db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
