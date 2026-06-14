d979e98d2616d137eb70e71f6ea3d6bb4892053d
Muhammad Arief Huda <ariefhuda434@gmail.com>
2026-06-14 14:07:39 +0700

    fix: force ipv4 for smtp connection on railway

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,7 @@ const createTransporter = () => nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
-  family: 4, // force IPv4, Railway gak support IPv6 outbound
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
